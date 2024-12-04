46c78d3f5b31751254240ed18aaa5f71b021fede
Alex <salmon_alexandre@outlook.fr>
2024-12-04 13:54:37 +0100

    <fix> connexio,.js & login.js néttoyé

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -5,10 +5,8 @@ export async function ajoutListenerMotdepasse(email, password) {
             email,
             password,
         }
-        console.log("test retour payload", data);
         const dataJS = JSON.stringify(data);
-        //const dataJ 
-        console.log("test retour charge utile", dataJS);            
+        //const dataJ         
             //const response = await fetch(`$API_URL/users/login`);
             const response = await fetch("http://localhost:5678/api/users/login", {
                 method: "POST",
@@ -18,12 +16,10 @@ export async function ajoutListenerMotdepasse(email, password) {
             console.log("reste reponse", response)
             if (response.ok){
                 const succes = await response.json();
-                console.log("YES", succes.token);
                 return {token: succes.token};
             }
             else {
                 const fail = await response.json();
-                console.log("NO", fail);
                 return  { error: fail.message || "Identifiants de connexion incorrect" };
             }   
 }
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -9,41 +9,16 @@ my_form.addEventListener('submit',async (e) => {
     // réinitialise la fonction de submit quand il y a des érreurs
     error_message.innerText = "";
     e.preventDefault();
-    const errors = validateForm (mail_input.value, password_input.value)
-    if(errors.length > 0){
-        error_message.innerText = errors.join(". ")
-    }
-    console.log("Email Input:", mail_input.value);
-    console.log("Password Input:", password_input.value);
 
     const api = await ajoutListenerMotdepasse(mail_input.value, password_input.value);
 
     if (api.error) {
         error_message.innerText = "Identifiants incorrects";
-        console.log("Erreur de connexion", api.error);
     }
 
     else {
-        console.log("Connecté !!");
         sessionStorage.setItem("token", api.token);
         alert ("Vous êtes connecté!");
         window.location.href = "index.html";
-        console.log("test token", api.token)
     }
-
-    console.log("Email Input 2:", mail_input.value);
-    console.log("Password Input 2:", password_input.value);
 })
-
-function validateForm (formmail, formpassword){
-    const errors = [];
-    if (formmail.trim() === "") {
-        errors.push("Mettez un email");
-        mail_input.parentElement.classList.add("Incorrect");
-    }
-    if(formpassword.trim() === ""){
-        errors.push("Mettez un mot de passe")
-        password_input.parentElement.classList.add('Incorrect');
-    }
-    return errors;
-}
